f6114100c68b279453e177aeea036161ea3ddbe2
GFHF Developer <developer@globalfootballhopefund.org>
2026-07-21 07:00:36 +0100

    Update firebase-config with exact raw Firebase console keys

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZZwZUTHqCqCvptGnLB3Vzq-ZGWPHEZ7o",
+  apiKey: "AIzaSyBZZwZUTHqCqCvpIGnlB3Vzq-ZGWPH0Z7o",
   authDomain: "global-football-hope-fund.firebaseapp.com",
   projectId: "global-football-hope-fund",
   storageBucket: "global-football-hope-fund.firebasestorage.app",
   messagingSenderId: "861980418289",
-  appId: "1:861980418289:web:365d21bead8c98f579c959",
-  measurementId: "G-YHHC0V89D3"
+  appId: "1:861980418289:web:70e9cd3c7b28411c79c950",
+  measurementId: "G-T93FM6CP1K"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
